b462babbee0ee7116daf6a6d132ccaeb39d8ed4c
Vaibhav Kaushal <85981945+VaibhavK1995@users.noreply.github.com>
2022-06-04 21:30:48 +0530

    Delete redapp directory

--- redapp/apps.py
+++
@@ -1,6 +0,0 @@
-from django.apps import AppConfig
-
-
-class RedappConfig(AppConfig):
-    default_auto_field = 'django.db.models.BigAutoField'
-    name = 'redapp'
--- redapp/forms.py
+++
@@ -1,9 +0,0 @@
-from django.forms import ModelForm, TextInput
-from .models import location
-
-
-class CityForm(ModelForm):
-    class Meta:
-        model = location
-        fields = ['name']
-        widgets = {'name': TextInput(attrs={'class': 'input', 'placeholder': 'City Name'})}
